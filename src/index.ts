c0c447592b40e17a91c38a097d36572b69945e96
Konsta Purtsi <konsta@identio.fi>
2021-09-18 23:05:50 +0300

    Set up express dev environment.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+const port: number = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world');
+});
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port}`);
+});
